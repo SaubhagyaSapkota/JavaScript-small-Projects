23bdae7277c7f00011c68f83636ad11b06b89c8b
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2025-07-10 18:01:56 +0545

    little changes on button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import Circle from "./components/circle";
+import Button from "./components/button";
 
 const App = () => {
   return (
-    <div>
-      <Circle />
+    <div className="button-container">
+      <Button variant="primary" size="sm">
+        Primary
+      </Button>
+
+      <Button variant="secondary" size="sm">
+        Secondary
+      </Button>
+
+      <Button variant="outline" size="lg">
+        Outline
+      </Button>
+
+      <Button variant="text" size="sm">
+        Text
+      </Button>
+
+      <Button variant="rounded" size="lg">
+        Rounded
+      </Button>
+
+      <Button variant="squared" size="md">
+        Squared
+      </Button>
+
+      <Button variant="primary" disabled>
+        Disabled
+      </Button>
     </div>
   );
 };
